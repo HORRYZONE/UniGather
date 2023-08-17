c50fb1f76cdfe0c4fe7fe84075b0f84c89b5af2a
HORRYZONE <danishfitri131@gmail.com>
2023-08-18 06:19:51 +0800

    add form_container_widget.dart implement in login_page.dart

--- unigather/lib/features/user_auth/presentation/pages/login_page.dart
+++ unigather/lib/features/user_auth/presentation/pages/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:unigather/features/user_auth/presentation/widgets/form_container_widget.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
@@ -10,10 +11,25 @@ class LoginPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Login'),
       ),
-      body: Column(
-        children: [
-          Text("Login", style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold),)
-        ],
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Login", style: TextStyle(fontSize: 37, fontWeight: FontWeight.bold)),
+            SizedBox(
+              height: 303,
+            ),
+            FormContainerWidget(
+              hintText: "Email",
+              isPasswordField: false,
+            ),
+            SizedBox(height: 10,),
+            FormContainerWidget(
+              hintText: "Password",
+              isPasswordField: true,
+            ),
+          ],
+        ),
       ),
     );
   }
