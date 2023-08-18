1c31f25333674b190ea98623fb5a91016c0749ad
HORRYZONE <danishfitri131@gmail.com>
2023-08-18 08:41:27 +0800

    add firebase auth service edit sign up function

--- unigather/lib/features/user_auth/presentation/pages/signup_page.dart
+++ unigather/lib/features/user_auth/presentation/pages/signup_page.dart
@@ -1,11 +1,31 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:unigather/features/user_auth/firebase_auth_services.dart';
 import 'package:unigather/features/user_auth/presentation/widgets/form_container_widget.dart';
 
 import 'login_page.dart';
 
-class SignUpPage extends StatelessWidget {
+class SignUpPage extends StatefulWidget {
   const SignUpPage({super.key});
+
+  @override
+  State<SignUpPage> createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  final FirebaseAuthService _auth = FirebaseAuthService();
+  TextEditingController _usernameController = TextEditingController();
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    _usernameController.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,40 +44,46 @@ class SignUpPage extends StatelessWidget {
               const SizedBox(
                 height: 30,
               ),
-              const FormContainerWidget(
+              FormContainerWidget(
+                controller: _usernameController,
                 hintText: "Username",
                 isPasswordField: false,
               ),
               const SizedBox(
                 height: 10,
               ),
-              const FormContainerWidget(
+              FormContainerWidget(
+                controller: _emailController,
                 hintText: "Email",
                 isPasswordField: false,
               ),
               const SizedBox(
                 height: 10,
               ),
-              const FormContainerWidget(
+              FormContainerWidget(
+                controller: _passwordController,
                 hintText: "Password",
                 isPasswordField: true,
               ),
               const SizedBox(
                 height: 30,
               ),
-              Container(
-                width: double.infinity,
-                height: 45,
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                  borderRadius: BorderRadius.circular(10),
+              GestureDetector(
+                onTap: _signUp,
+                child: Container(
+                  width: double.infinity,
+                  height: 45,
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: const Center(
+                      child: Text(
+                    "Sign Up",
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.bold),
+                  )),
                 ),
-                child: const Center(
-                    child: Text(
-                  "Sign Up",
-                  style: TextStyle(
-                      color: Colors.white, fontWeight: FontWeight.bold),
-                )),
               ),
               const SizedBox(
                 height: 20,
@@ -89,5 +115,20 @@ class SignUpPage extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  void _signUp() async {
+    String username = _usernameController.text;
+    String email = _emailController.text;
+    String password = _passwordController.text;
+
+    User? user = await _auth.signUpWithEmailAndPassword(email, password);
+
+    if (user != null) {
+      print("User is successfully created");
+      Navigator.pushNamed(context, "/home");
+    } else {
+      print("Some error happend");
+    }
   }
 }
